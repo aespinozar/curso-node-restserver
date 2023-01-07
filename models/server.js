274e5022e2aa95cd98da36f2f6e37065c7317a3e
David Espinoza <aespinozar@unmsm.edu.pe>
2023-01-06 23:31:14 -0500

    Fin Seccion 9

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
 
@@ -7,12 +8,18 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
+        //llamada a conectarDB
+        this.conectarDB();
         //Middelware
         this.middlewares();
         //Rutas de la aplicacion
         this.routes();
         
 
+    }
+
+    async conectarDB() {
+        await dbConnection();
     }
     
     //aque se define el metodo middleware que publicara la carpeta public
